4fedc26d9e77e9bca1350c7439b54bd9c7b0e472
olovhax <olovhax@gmail.com>
2022-09-14 11:01:32 -0600

    added new codewars solution

---
+++ 8kyu/reversedStrings.js
@@ -0,0 +1,9 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+// STRINGS/FUNDAMENTALS
+
+// My solution
+
+const solution = str => str.split("").reverse().join("")
