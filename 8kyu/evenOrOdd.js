c85d302623d8e02e1018bb7dce9a462d5ba0d09e
olovhax <olovhax@gmail.com>
2022-09-18 11:12:56 -0600

    added new codewars solution

---
+++ 8kyu/evenOrOdd.js
@@ -0,0 +1,5 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// MATHEMATICS/FUNDAMENTALS
+
+const even_or_odd = n => (n % 2 === 0) ? "Even":"Odd"
